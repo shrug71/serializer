eebcc2e8c8194e750a06e613a0ddb1e9dca9e929
jmw <shashiknt.dubey@octavesoftwares.com>
2023-10-27 15:32:21 +0530

    demo api

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+# app/controllers/posts_controller.rb
+class PostsController < ApplicationController
+   skip_before_action :verify_authenticity_token, only: [:create,:update,:destroy]
+   
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
